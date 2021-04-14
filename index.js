5ca5f03fbed3ac495ae586979a66b7e7843c5cb6
David Colatti <davidcolatti@gmail.com>
2021-04-14 14:37:55 -0400

    feat: added state

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const phone = require("phone");
+const zipcodes = require("zipcodes");
 const csvOrExcel = require("csv-excel-to-json");
+const cityZipByPhone = require("city-zip-by-phone");
 const { convertArrayToCSV } = require("convert-array-to-csv");
 
 const writeJson = async () => {
   try {
-    csvOrExcel.convertToJson("./input.csv", "./data.json");
+    csvOrExcel.convertToJson("./data.csv", "./data.json");
 
     return true;
   } catch (e) {
@@ -24,17 +26,31 @@ const main = async () => {
       const newData = data.map((lead) => {
         const { phone_number } = lead;
         const info = phone(phone_number);
-        let cleanedNum;
+        let cleanedNum = "";
 
         if (info[0]) {
           cleanedNum = info[0].replace(/^\+1/g, "");
         }
 
-        return {
+        const location = cityZipByPhone(cleanedNum);
+
+        const zip_code = location.zip;
+
+        if (zip_code) {
+          const zip_code_data = zipcodes.lookup(zip_code);
+          location.state = zip_code_data.state;
+        }
+
+        let output = {
           ...lead,
           phone_number: cleanedNum,
-          countryCode: info[1],
+          city: location.city,
+          state: location.state,
+          zip: zip_code,
+          countryCode: info[1] || "",
         };
+
+        return output;
       });
 
       const filteredData = newData.filter((lead) => {
@@ -52,7 +68,7 @@ const main = async () => {
       fs.writeFile("./output.csv", csv, (err) => {
         console.log(err || "Done");
       });
-    }, 500);
+    }, 3000);
   } catch (e) {
     console.log("Failed");
   }
