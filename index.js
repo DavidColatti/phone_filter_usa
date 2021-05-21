4be1515b6f8bdb5f030679893f646dc3d3786a95
David Colatti <davidcolatti@gmail.com>
2021-05-21 09:39:58 -0400

    feat; refactored to use personal npm library

--- index.js
+++ index.js
@@ -1,78 +1,64 @@
 const fs = require("fs");
 const phone = require("phone");
 const zipcodes = require("zipcodes");
-const csvOrExcel = require("csv-excel-to-json");
 const cityZipByPhone = require("city-zip-by-phone");
-const { convertArrayToCSV } = require("convert-array-to-csv");
-
-const writeJson = async () => {
-  try {
-    csvOrExcel.convertToJson("./data.csv", "./data.json");
-
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
+const { csvFileToArray, arrayToCsvFile } = require("csv-to-and-from-array");
 
 const main = async () => {
   try {
-    let data;
-    await writeJson();
+    const data = await csvFileToArray({
+      filePath: "./data.csv",
+      callback: () => console.log("Converted csv to array"),
+    });
 
-    await setTimeout(async () => {
-      data = require("./data.json");
+    const newData = data.map((lead) => {
+      const { phone_number } = lead;
+      const info = phone(phone_number);
+      let cleanedNum = "";
 
-      const newData = data.map((lead) => {
-        const { phone_number } = lead;
-        const info = phone(phone_number);
-        let cleanedNum = "";
+      if (info[0]) {
+        cleanedNum = info[0].replace(/^\+1/g, "");
+      }
 
-        if (info[0]) {
-          cleanedNum = info[0].replace(/^\+1/g, "");
+      const location = cityZipByPhone(cleanedNum);
+
+      const zip_code = location.zip;
+
+      let state = "";
+      if (zip_code) {
+        const zip_code_data = zipcodes.lookup(zip_code);
+
+        if (zip_code_data) {
+          state = zip_code_data.state;
         }
+      }
 
-        const location = cityZipByPhone(cleanedNum);
+      let output = {
+        ...lead,
+        phone_number: cleanedNum,
+        city: location.city,
+        state: state,
+        zip: zip_code,
+        countryCode: info[1] || "",
+      };
 
-        const zip_code = location.zip;
+      return output;
+    });
 
-        let state = "";
-        if (zip_code) {
-          const zip_code_data = zipcodes.lookup(zip_code);
+    const filteredData = newData.filter((lead) => {
+      if (!!lead.phone_number === false) {
+        return false;
+      } else {
+        return lead.countryCode === "USA" || lead.countryCode === undefined;
+      }
+    });
 
-          if (zip_code_data) {
-            state = zip_code_data.state;
-          }
-        }
-
-        let output = {
-          ...lead,
-          phone_number: cleanedNum,
-          city: location.city,
-          state: state,
-          zip: zip_code,
-          countryCode: info[1] || "",
-        };
-
-        return output;
-      });
-
-      const filteredData = newData.filter((lead) => {
-        if (!!lead.phone_number === false) {
-          return false;
-        } else {
-          return lead.countryCode === "USA" || lead.countryCode === undefined;
-        }
-      });
-
-      console.log(`Found ${filteredData.length} out of ${data.length}`);
-
-      const csv = await convertArrayToCSV(filteredData);
-
-      fs.writeFile("./output.csv", csv, (err) => {
-        console.log(err || "Done");
-      });
-    }, 3000);
+    arrayToCsvFile({
+      data: filteredData,
+      filePath: "./output.csv",
+      callback: () =>
+        console.log(`Found ${filteredData.length} out of ${data.length}`),
+    });
   } catch (e) {
     console.log("Failed");
   }
